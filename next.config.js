d61d2a7aff8a24ef54958c02a93ea1f576cd3ecd
Mikhailov Aleksandr <sashkatawer228@gmail.com>
2023-07-28 04:51:14 +0600

    fix

--- next.config.js
+++ next.config.js
@@ -1,20 +1,27 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  /**
+   * Enable static exports for the App Router.
+   *
+   * @see https://nextjs.org/docs/pages/building-your-application/deploying/static-exports
+   */
   output: 'export',
-  typescript: { ignoreBuildErrors: false },
-  experimental: {
-    appDir: true,
-    serverActions: true,
-  },
+
+  /**
+   * Set base path. This is usually the slug of your repository.
+   *
+   * @see https://nextjs.org/docs/app/api-reference/next-config-js/basePath
+   */
+  basePath: '/nextjs-github-pages',
+
+  /**
+   * Disable server-based image optimization. Next.js does not support
+   * dynamic features with static exports.
+   *
+   * @see https://nextjs.org/docs/pages/api-reference/components/image#unoptimized
+   */
   images: {
-    domains: ['picsum.photos', '127.0.0.1'],
-  },
-  webpack(config) {
-    config.module.rules.push({
-      test: /\.svg$/,
-      use: ['@svgr/webpack'],
-    })
-    return config
+    unoptimized: true,
   },
 }
 
