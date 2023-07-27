3f3e617c8d48a09345338d4e7b8e3bacd3f10274
Mikhailov Aleksandr <sashkatawer228@gmail.com>
2023-07-28 04:39:15 +0600

    fix: add config for nextjs

--- next.config.js
+++ next.config.js
@@ -1,4 +1,20 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  typescript: { ignoreBuildErrors: false },
+  experimental: {
+    appDir: true,
+    serverActions: true,
+  },
+  images: {
+    domains: ['picsum.photos', '127.0.0.1'],
+  },
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack'],
+    })
+    return config
+  },
+}
 
 module.exports = nextConfig
